5ea01c92c986b1c50fdc907f4fd5efc2d0ab27f4
Oz <ozshalem98@gmail.com>
2024-03-01 16:06:21 +0100

    added react router + dashboard and income statements

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,13 +1,16 @@
-import React, { ChangeEvent, FormEvent, SyntheticEvent } from 'react'
+import React, { ChangeEvent, FormEvent, SyntheticEvent } from "react";
 
 interface Props {
-    search: string | undefined;
-    handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
-    onSearchSubmit: (e: SyntheticEvent) => void;
+  search: string | undefined;
+  handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSearchSubmit: (e: SyntheticEvent) => void;
 }
 
-const Search: React.FC<Props> = ({search, onSearchSubmit,  handleSearchChange}: Props): JSX.Element => {
-    
+const Search: React.FC<Props> = ({
+  search,
+  onSearchSubmit,
+  handleSearchChange,
+}: Props): JSX.Element => {
   return (
     <section className="relative bg-gray-100">
       <div className="max-w-4xl mx-auto p-6 space-y-6">
@@ -25,7 +28,7 @@ const Search: React.FC<Props> = ({search, onSearchSubmit,  handleSearchChange}: 
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,23 +1,34 @@
 import axios from "axios";
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 
 interface searchResponse {
-    data: CompanySearch[];
+  data: CompanySearch[];
 }
 
 export const searchCompanies = async (query: string) => {
-    try {
-        const data = axios.get<searchResponse>(
-            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
-        );
-        return data;
-    } catch (error) {
-        if(axios.isAxiosError(error)) {
-            console.log("error message:", error.message);
-            return error.message;
-        }else {
-            console.log("unexpected error.");
-            return "unexpected error has occurred.";
-        }
+  try {
+    const data = await axios.get<searchResponse>(
+      `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`,
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message:", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error.");
+      return "unexpected error has occurred.";
     }
-}
+  }
+};
+
+export const getCompanyProfile = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
